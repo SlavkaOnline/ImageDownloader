2d532ffa4a220dbbcd2f3f27a9d772e9ec7b509b
= <=>
2017-10-25 21:56:42 +0500

    Дописал тест на создание диалога об ошибке

--- ImageDownloader.Tests/ImageDownloaderViewModelTests.cs
+++ ImageDownloader.Tests/ImageDownloaderViewModelTests.cs
@@ -1,9 +1,14 @@
 ﻿using ImageDownloader.Models;
+using ImageDownloader.Models.Interfaces;
 using ImageDownloader.ViewModels;
+using ImageDownloader.ViewModels.Interfaces;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +24,26 @@ namespace ImageDownloader.Tests
             imageDownloaderViewModel.Url = @"http://lifeandjoy.ru/uploads/posts/2015-07/1438256217_lifeandjoy.ru_02.jpg";
             await imageDownloaderViewModel.StartDownloadAsync();
             Assert.IsNotNull(imageDownloaderViewModel.SourceImage);
+        }
+        
+        [Test]
+        public async Task CreateExceptionMessageWhenDownloadFailed()
+        {
+            var mockFileDownLoader = Substitute.For<IFileDownloader>();
+            mockFileDownLoader
+                .Download(Arg.Any<string>(), Arg.Any<Action<double>>())
+                .Throws(new WebException());
+
+            var isCReated = false;
+            var mockViewFactory = Substitute.For<IViewFactory>();
+            mockViewFactory
+                .When(x => x.CreateExceptionViewDialogAsync(Arg.Any<string>()))
+                .Do( _ => isCReated = true);
+
+            var imageDownloaderViewModel = new ImageDownloaderViewModel(mockFileDownLoader, mockViewFactory);
+            await imageDownloaderViewModel.StartDownloadAsync();
+
+            Assert.That(isCReated, Is.True);
         }
 
     }
